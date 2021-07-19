7d0ebb8b61a0d232d263b4bf9dc457ac5febefe3
GloryCiel <gloryciel@knu.ac.kr>
2021-07-20 02:42:12 +0900

    Recovery Success

---
+++ app/src/main/java/com/example/letter/LoginActivity.kt
@@ -0,0 +1,84 @@
+package com.example.letter
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import com.example.letter.databinding.LoginBinding
+
+class LoginActivity : AppCompatActivity()
+{
+    private lateinit var binding: LoginBinding//LoginActivity & login.xml를 bind
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = LoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.login.setEnabled(false)//이름, 학번, 학과를 다 입력하기 전에 버튼 못누르게 함
+        setContentView(view)//화면 출력
+
+        var name = ""
+        var number = ""
+        var major = ""
+
+        (binding.editText3).addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                name+=p0
+                if((name.equals("")||number.equals("")||major.equals(""))==false)//타이핑시 마지막으로 작동
+                    binding.login.setEnabled(true)
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {//타이핑 시 2번째로 작동
+                if((name.equals("")||number.equals("")||major.equals(""))==false)
+                    binding.login.setEnabled(true)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {//타이핑 시 제일 먼저 작동
+                if((name.equals("")||number.equals("")||major.equals(""))==false)
+                    binding.login.setEnabled(true)
+            }
+        })
+
+        (binding.editText).addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                number+=p0
+                if((name.equals("")||number.equals("")||major.equals(""))==false)
+                    binding.login.setEnabled(true)
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if((name.equals("")||number.equals("")||major.equals(""))==false)
+                    binding.login.setEnabled(true)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if((name.equals("")||number.equals("")||major.equals(""))==false)
+                    binding.login.setEnabled(true)
+            }
+        })
+
+        (binding.editText2).addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                major+=p0
+                if((name.equals("")||number.equals("")||major.equals(""))==false)
+                    binding.login.setEnabled(true)
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if((name.equals("")||number.equals("")||major.equals(""))==false)
+                    binding.login.setEnabled(true)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if((name.equals("")||number.equals("")||major.equals(""))==false)
+                    binding.login.setEnabled(true)
+            }
+        })
+
+        binding.login.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))//버튼이 눌리면 로그인 이후 화면으로 이동
+        }
+    }
+}
