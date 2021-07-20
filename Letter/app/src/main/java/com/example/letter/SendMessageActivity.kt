03d9cf0407b2f919397dcafb2b01ea1391947377
kimgihong2510 <kimgihong2510@gmail.com>
2021-07-20 19:27:52 +0900

    send message activity까지 전체 완성

--- Letter/app/src/main/java/com/example/letter/SendMessageActivity.kt
+++ Letter/app/src/main/java/com/example/letter/SendMessageActivity.kt
@@ -1,7 +1,11 @@
 package com.example.letter
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import com.example.letter.databinding.ActivitySendMessageBinding
 import java.time.chrono.JapaneseEra.values
@@ -10,6 +14,7 @@ import java.time.chrono.JapaneseEra.values
 class SendMessageActivity : AppCompatActivity() {
     private lateinit var binding:ActivitySendMessageBinding
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_message)
@@ -30,11 +35,30 @@ class SendMessageActivity : AppCompatActivity() {
         binding.MinPicker.minValue=0
         binding.MinPicker.maxValue=59
 
+        //쪽찌 종류
         val purpose= arrayOf("친목", "멘토", "멘티", "취준")
         val myAdapter=ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, purpose)
         binding.SetColor.adapter=myAdapter
 
+        //쪽찌마다 색깔바뀌게 만들기
+        binding.SetColor.onItemSelectedListener=object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if(id.toInt()==0){
+                    binding.letter.setBackgroundResource(R.color.purpose1)}
+                if(id.toInt()==1){
+                    binding.letter.setBackgroundResource(R.color.purpose2)}
+                if(id.toInt()==2){
+                    binding.letter.setBackgroundResource(R.color.purpose3)}
+                if(id.toInt()==3){
+                    binding.letter.setBackgroundResource(R.color.purpose4)}
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
+
+        //프로필 사진
         binding.ProfilePicture.setImageResource(R.drawable.ic_launcher_background)
     }
-
 }
