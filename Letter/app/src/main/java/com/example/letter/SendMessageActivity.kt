e0cce19b233d5dc245807492bd29ba9b69556655
kimgihong2510 <kimgihong2510@gmail.com>
2021-07-20 10:57:18 +0900

    title bar 없앰, AndroidManifest 되돌려놓음.

--- Letter/app/src/main/java/com/example/letter/SendMessageActivity.kt
+++ Letter/app/src/main/java/com/example/letter/SendMessageActivity.kt
@@ -2,10 +2,31 @@ package com.example.letter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.letter.databinding.ActivitySendMessageBinding
 
 class SendMessageActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySendMessageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_message)
+
+        binding= ActivitySendMessageBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+        //인원수, 시간, 분 롤
+        binding.PersonPicker.minValue=0
+        binding.PersonPicker.maxValue=10
+        val ArrayPicker= arrayOf("제한 없음", "1명", "2명", "3명", "4명", "5명", "6명", " 7명", "8명", "9명", "10명")
+        binding.PersonPicker.displayedValues= ArrayPicker
+
+        binding.HourPicker.minValue=0
+        binding.HourPicker.maxValue=30
+
+        binding.MinPicker.minValue=0
+        binding.MinPicker.maxValue=59
+
     }
+
 }
