d8ca2df7487e7aed1a426e580f0c08cc172028a1
kimgihong2510 <kimgihong2510@gmail.com>
2021-07-21 19:32:11 +0900

    밤 theme 없음. 밤 theme있으니까 글자 안보이는 현상 있어서 그냥 뺍시다

--- Letter/app/src/main/java/com/example/letter/LoginActivity.kt
+++ Letter/app/src/main/java/com/example/letter/LoginActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.letter.databinding.LoginBinding
 
 class LoginActivity : AppCompatActivity()
