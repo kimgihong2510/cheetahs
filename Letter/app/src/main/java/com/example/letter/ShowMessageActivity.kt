ccda2469e42411184957f4d388b92d1aa65ebc3d
kimgihong2510 <kimgihong2510@gmail.com>
2021-07-22 17:56:37 +0900

    서버통신 다 만들었음

--- Letter/app/src/main/java/com/example/letter/ShowMessageActivity.kt
+++ Letter/app/src/main/java/com/example/letter/ShowMessageActivity.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.letter.databinding.ActivitySendMessageBinding
 import com.example.letter.databinding.ActivityShowMessageBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ShowMessageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityShowMessageBinding
@@ -25,6 +30,32 @@ class ShowMessageActivity : AppCompatActivity() {
             val nextIntent= Intent(this, ShowProfileActivity::class.java)
             startActivity(nextIntent)
         }
+
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://25.61.78.177:8080/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val api = retrofit.create(Connect.GETshowMessage::class.java)!!
+        var letters=api.showMessage(2) // message ID
+
+        letters.enqueue(object : Callback<Connect.ShowMessageStruct> {
+            override fun onResponse(
+                call: Call<Connect.ShowMessageStruct>,
+                response: Response<Connect.ShowMessageStruct>
+            ) {
+                var tmp=response.body() as Connect.ShowMessageStruct
+                println(tmp.data[1].id)
+            }
+
+            override fun onFailure(call: Call<Connect.ShowMessageStruct>, t: Throwable) {
+                println("안됨")
+                //////////////
+            }
+        })
+
     }
 
 
